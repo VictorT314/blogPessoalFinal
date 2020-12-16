e0dc677b778668d35d1d772d7f1a38657e7dbe49
Victor Tavares <victor.as156@gmail.com>
2020-12-16 12:48:52 -0300

    heroku

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -12,11 +12,11 @@ export class AuthService {
   constructor(private http: HttpClient) { }
 
   logar(userLogin: UserLogin) {
-    return this.http.post('http://localhost:8080/usuarios/logar', userLogin)
+    return this.http.post('https://blogpessoal-turma11-victor.herokuapp.com/usuarios/logar', userLogin)
   }
 
   cadastrar(user: User) {
-    return this.http.post('http://localhost:8080/usuarios/cadastrar', user)
+    return this.http.post('https://blogpessoal-turma11-victor.herokuapp.com/usuarios/cadastrar', user)
   }
 
   btnSair(){
