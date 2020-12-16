4ce9ead17b0478321b3f4addbe4755ad04945b1b
Victor Tavares <victor.as156@gmail.com>
2020-12-16 12:07:37 -0300

    Versão final Blog Pessoal

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,32 @@
+import { UserLogin } from './../model/UserLogin';
+import { Router } from '@angular/router';
+import { AuthService } from './../service/auth.service';
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment.prod';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  userLogin: UserLogin = new UserLogin()
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+  }
+
+  entrar() {
+    this.authService.logar(this.userLogin).subscribe((resp: UserLogin) => {
+      this.userLogin = resp
+      environment.token = this.userLogin.token
+      this.router.navigate(['/feed'])
+    })
+  }
+
+}
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,26 @@
+import { AuthService } from './../service/auth.service';
+import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment.prod';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  constructor(
+    private router: Router,
+    public auth: AuthService
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  sair() {
+    this.router.navigate(['/login'])
+    environment.token = ''
+  }
+
+}
